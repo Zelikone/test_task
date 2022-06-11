a56d0e6b52bc33c335431ea602b8a558f5ee475a
zelenov <mykyta.zelenov@nure.ua>
2022-06-11 15:15:18 +0300

    updatet burger button

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,16 @@
+$(window).click(function() {
+	$('.header__mob-menu').removeClass('show');
+	$('.header__mob').removeClass('show-menu');
+	$('.burger-button').removeClass('active');
+  });
+
 $('.burger-button').on('click', function () {
+	$(this).toggleClass('active');
 	$('.header__mob-menu').toggleClass('show');
 	$('.header__mob').toggleClass('show-menu');
+});
+
+  
+$('.burger-button').click(function(event){
+	event.stopPropagation();
 });
